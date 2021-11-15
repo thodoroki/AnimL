5aa243f59807b0a3e560a9b7d1975b19c3ee4444
Epaminondas Noronha Feitosa <nondasnoronha@gmail.com>
2021-11-15 09:59:12 -0300

    Add botao remover funcional

--- client/src/components/favorite-animes/listfavorite.js
+++ client/src/components/favorite-animes/listfavorite.js
@@ -3,6 +3,16 @@ import './style.css';
 import api from '../../services/api'
 import Card from '../anime/cardAnime';
 
+
+async function handleDelete(animeId){
+    if(window.confirm("Deseja realmente remover este anime dos seus favoritos?")){
+        var result = await api.delete(`/api/favoritos/delete/${animeId}`)
+        if(result.status == 200){
+            window.location.href = "/favorites"
+        }
+    }
+}
+
 function FavoritesList() {
     const [animes, setAnimes] = useState({});
     useEffect(() => {
@@ -18,10 +28,13 @@ function FavoritesList() {
                     <ul className="animes-list">
                         {animes.data.map((anime, index) => (
                             <li key={index}>
-                                <Card titulo= {anime.nome_anime}
-                                    poster={anime.poster}
-                                    animeId={anime.id_original}
+                                <div className="favorite-card">
+                                    <Card titulo= {anime.nome_anime}
+                                        poster={anime.poster}
+                                        animeId={anime.id_original}
                                     />
+                                    <button className="button__remove__favorite"onClick={() => handleDelete( anime._id )} >Remover</button>
+                                </div>
                             </li>
                         ))}
                     </ul>
