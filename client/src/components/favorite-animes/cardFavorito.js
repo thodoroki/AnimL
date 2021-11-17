b97119fbabed487e5a507f9227f38bb7d5f38e20
NondasNF <nondasnoronha@gmail.com>
2021-11-16 21:47:17 -0300

    add correction

--- client/src/components/favorite-animes/cardFavorito.js
+++ client/src/components/favorite-animes/cardFavorito.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import './card.css';
+import api from '../../services/api'
+
+async function handleDelete(animeId) {
+    if (window.confirm("Deseja realmente remover este anime dos seus favoritos?")) {
+        var result = await api.delete(`/api/favoritos/delete/${animeId}`)
+        if (result.status == 200) {
+            window.location.href = "/favorites"
+        }
+    }
+}
 
 export default function CardAnime(props){
     return (
@@ -8,7 +18,7 @@ export default function CardAnime(props){
             <div className="card--info">
                 <h3>{props.titulo}</h3>
             </div>
-            <button>Remover</button>
+            <button className="button__remove__favorite"onClick={() => handleDelete( props.originalId )} >Remover</button>
             
         </div>
     );
--- client/src/components/favorite-animes/listfavorite.js
+++ client/src/components/favorite-animes/listfavorite.js
@@ -4,15 +4,6 @@ import api from '../../services/api'
 import CardFavoritos from './cardFavorito';
 
 
-async function handleDelete(animeId){
-    if(window.confirm("Deseja realmente remover este anime dos seus favoritos?")){
-        var result = await api.delete(`/api/favoritos/delete/${animeId}`)
-        if(result.status == 200){
-            window.location.href = "/favorites"
-        }
-    }
-}
-
 function FavoritesList() {
     const [animes, setAnimes] = useState({});
     useEffect(() => {
@@ -21,27 +12,25 @@ function FavoritesList() {
             setAnimes(response);
         }
         loadAnimes();
-    }, []);    
-        return (
-            <div className="List">
-                {animes.data && (
-                    <ul className="animes-list">
-                        {animes.data.map((anime, index) => (
-                            <li key={index}>
-                                <div className="favorite-card">
-                                    <Card titulo= {anime.nome_anime}
-                                        poster={anime.poster}
-                                        animeId={anime.id_original}
-                                    />
-                                    <button className="button__remove__favorite"onClick={() => handleDelete( anime._id )} >Remover</button>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                )};
+    }, []);
+    return (
+        <div className="List">
+            {animes.data && (
+                <ul className="animes-list">
+                    {animes.data.map((anime, index) => (
+                        <li key={index}>
+                            <CardFavoritos titulo={anime.nome_anime}
+                                poster={anime.poster}
+                                animeId={anime.id_original}
+                                originalId={anime._id}
+                            />
+                        </li>
+                    ))}
+                </ul>
+            )};
 
-            </div>
-        );
-    }
+        </div>
+    );
+}
 
-    export default FavoritesList;
+export default FavoritesList;
